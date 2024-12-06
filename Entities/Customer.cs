e2d3ac4eb0df99ac9f574c9c81e5d4241b7895d0
Your Name <zohrsadeghi@gmail.com>
2024-12-06 08:26:41 +0100

    Added classes customer and supplier

---
+++ Entities/Customer.cs
@@ -0,0 +1,11 @@
+namespace FelisEShop.api.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
--- Entities/Product.cs
+++ Entities/Product.cs
@@ -1,5 +1,3 @@
-
-
 namespace FelisEShop.api.Entities
 {
     public class Product
